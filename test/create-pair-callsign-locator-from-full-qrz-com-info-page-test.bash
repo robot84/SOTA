f7b43c7ef8f040b5d21b70963794de2ac491c633
robot84 <zabkiewicz.robert@gmail.com>
2018-11-02 19:34:57 +0100

    @#

---
+++ test/create-pair-callsign-locator-from-full-qrz-com-info-page-test.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+t001_cookie_validation() {
+
+rm SO9ARC/database/SQ9KCN.log
+src/get-callsign-info-page.bash SQ9KCN >> /dev/null
+src/create-pair-callsign-locator-from-full-qrz-com-info-page.bash SQ9KCN | \
+grep '??????' >> /dev/null \
+&& echo Failed || echo Passed
+}
+
+t002_cookie_validation() {
+
+rm SO9ARC/database/SQ9KCN.log
+src/get-callsign-info-page.bash SQ9KCN >> /dev/null
+src/create-pair-callsign-locator-from-full-qrz-com-info-page.bash SQ9KCN | \
+grep 'KO00ac' >> /dev/null \
+&& echo Passed || echo Failed
+}
+
+
+main() {
+cd $(dirname $0)/..
+. resources/config.cfg
+
+t001_cookie_validation
+t002_cookie_validation
+}
+
+main 
