17c713aad185500fa7d32ae27b3b4e76de6f6b96
robot84 <zabkiewicz.robert@gmail.com>
2018-11-03 16:38:42 +0100

    Add cookie validation in src/get-callsign-info-page.bash and 2 tests for that in test/get-callsign-info-page-test.bash

---
+++ src/get-callsign-info-page.bash
@@ -0,0 +1,153 @@
+#!/bin/bash
+#
+# (c) SO9ARC Robert Zabkiewicz 2018
+#
+# VER=0.1.2
+#$SLEEP_TIME - getting better performance against 500 Too Many Requests server's response. In seconds.
+#
+# Revision History:
+# 0.1.2 we don't accept stroked callsigns like W7ABC/x, etc
+# 0.1.3 different exit codes for different errors
+# 0.1.4 use config.cfg for loading information about database localization
+
+CONFIG_FILE="../resources/config.cfg"
+SLEEP_TIME=2
+ERROR__NO_CALLSIGN_PASSED_TO_SCRIPT=1
+ERROR__CALLSIGN_DOESNT_MATCH_PATTERN=2
+ERROR__COMMUNICATION_WITH_QRZ_COM_SERVER_FAILED=3
+ERROR__CANNOT_LOAD_CONFIG_FILE=4
+ERROR__CANNOT_OPEN_COOKIE_FILE_FOR_READING=5
+ERROR__INVALID_OR_EXPIRED_COOKIE_FILE=6
+
+### Functions ###
+
+# f_log_msg() - Logging a message to log file
+#
+# Arguments: log_filename message_to_log
+# Return: nothing
+#
+# Example; f_log_msg ../log/error.log "Error: Failure foo."
+#
+f_log_msg() {
+LOG_FILE_NAME="${1}"
+shift
+[ ! -e ${LOG_FILE_NAME} ] && \
+ ( mkdir -p $(dirname ${LOG_FILE_NAME}); touch ${LOG_FILE_NAME}; )
+
+LOG_DATE=`LC_ALL=C date "+%b %e %H:%M:%S"`
+LOG_HOST=`hostname -s`
+LOG_PROCESS=$(basename $0)
+LOG_PID=$BASHPID
+LOG_MSG="$@"
+echo "$LOG_DATE $LOG_HOST $LOG_PROCESS[$LOG_PID]: $LOG_MSG" >> "${LOG_FILE_NAME}"
+}
+
+### End of Functions ###
+
+### Main ###
+cd $(dirname $0)
+if [ -r ${CONFIG_FILE} ]
+then
+.  ${CONFIG_FILE}
+else
+echo "Cannot open config file for reading!"
+echo "Check if file exists and you have permissions to read it."
+echo "Path to file: ${CONFIG_FILE}"
+echo "Cannot continue. Exiting..."
+exit $ERROR__CANNOT_LOAD_CONFIG_FILE
+fi
+
+if [ ! -r ${COOKIE_FILENAME} ]
+then
+echo $COOKIE_FILENAME
+echo "Error: Cannot open cookie file for read."
+echo "Check if cookie file ${COOKIE_FILENAME} exists 
+and you have permissions for reading it."
+
+	f_log_msg $LOG_FILE "Error: no cookie file available. Cannot login to http://qrz.com/"
+exit $ERROR__CANNOT_OPEN_COOKIE_FILE_FOR_READING
+fi
+
+if [ "$#" -lt 1 ]
+then
+echo "$0: Mandatory argument ommited."
+echo "Try '$0 --help' for more information."
+	 f_log_msg $LOG_FILE ${!ERROR__NO_CALLSIGN_PASSED_TO_SCRIPT@}
+exit $ERROR__NO_CALLSIGN_PASSED_TO_SCRIPT
+fi
+
+POSITIONAL=()
+	while [[ $# -gt 0 ]]
+	do
+	key="$1"
+
+	case $key in
+	-h|--help)
+	echo ""
+	echo "Usage:"
+	echo "${0##*/} <callsign>"
+	echo
+exit $ERROR__NO_CALLSIGN_PASSED_TO_SCRIPT
+	;;
+	-*)
+	echo "$0: invalid option -- '$1'"
+	echo "Try '$0 --help' for more information."
+	exit
+	;;
+	*) 
+	POSITIONAL+=("$1") # save it in an array for later
+	shift
+	;;
+	esac
+	done
+	set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+CALLSIGN="$1"
+
+if [[ "$CALLSIGN" =~ [A-Za-z0-9]*/[A-Za-z0-9][A-Za-z0-9]* ]]
+then
+echo "Unacceptable (stroked) callsign $CALLSIGN. Exiting..."
+exit $ERROR__CALLSIGN_DOESNT_MATCH_PATTERN
+fi
+ 
+
+if [ ! -d "$DB_DIRECTORY" ]
+then
+	mkdir "$DB_DIRECTORY"
+fi
+
+if [ -e "${DB_DIRECTORY}/${CALLSIGN}.log" ]
+then
+	echo "Information about callsing $CALLSIGN already obtained. Nothing to do."
+else
+echo "Getting info about $CALLSIGN..."
+
+curl \
+-s --cookie ${COOKIE_FILENAME} \
+-X POST \
+-F "tquery=$CALLSIGN" \
+-F "mode=callsign" \
+-F "id=topcall" \
+-e "https://www.qrz.com/lookup" \
+-A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36' \
+"https://www.qrz.com/lookup"  | \
+html2text > "${DB_DIRECTORY}/${CALLSIGN}.log" || \
+{ echo "ERROR: Cannot fetch callsign's information."; exit $ERROR__COMMUNICATION_WITH_QRZ_COM_SERVER_FAILED; };
+
+grep -q Logout "${DB_DIRECTORY}/${CALLSIGN}.log"
+if [ $? -ne 0 ]
+then
+	f_log_msg $LOG_FILE "Error: expired or invalid cookie file. Cannot login to http://qrz.com/"
+	echo "Error: expired or invalid cookie file. 
+	Cannot login to http://qrz.com/"
+	rm "${DB_DIRECTORY}/${CALLSIGN}.log"
+	exit $ERROR__INVALID_OR_EXPIRED_COOKIE_FILE
+fi
+
+sleep $SLEEP_TIME
+
+fi
+
+cd - >> /dev/null
+exit 0
--- test/get-callsign-info-page-test.bash
+++ test/get-callsign-info-page-test.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
-#3 testy!
-#jeden jak zwraca ze nothing to do, a drugi jak sciaga info z serwera
-#wtedy mamy 100% pokrycia kodu i rozgalezien
-#a trzeci zeby usage pokazalo
-#w TDD bym to zrobil tak:
 
 t001_script_runned_without_arguments_return_1() {
 
 ../src/get-callsign-info-page.bash > /dev/null 2> /dev/null \
 && echo Failed || echo Passed
+rm  -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 
@@ -16,23 +12,49 @@ t002_script_has_this_callsign_in_db__is_it_consious_of_that() {
 cp files/SQ9IAW.log ${DB_DIRECTORY}
 ../src/get-callsign-info-page.bash SQ9IAW | grep \
 "Nothing to do." > /dev/null && echo Passed || echo Failed
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 t003_script_hasNOT_this_callsign_in_db__() {
 
 rm -f ${DB_DIRECTORY}/SQ9IAW.log
 ../src/get-callsign-info-page.bash SQ9IAW | grep -v "Nothing to do." > /dev/null && echo Passed || echo Failed
-
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 t004_script_hasNOT_this_callsign_in_db__() {
-rm  ${DB_DIRECTORY}/SQ9IAW.log
-../src/get-callsign-info-page.bash SQ9IAW | grep \
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
+../src/get-callsign-info-page.bash SQ9IAW | grep -q \
 "Getting info about" > /dev/null && echo Passed || echo Failed
 
-
-
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
+
+t005_cookie_validation() {
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
+TMP=$(mktemp)
+cp  "$COOKIE_FILENAME" $TMP
+cp files/good_cookie.txt  "$COOKIE_FILENAME"
+../src/get-callsign-info-page.bash SQ9IAW  > /dev/null
+grep -q JO90xc "${DB_DIRECTORY}/SQ9IAW.log" && \
+grep -q Logout "${DB_DIRECTORY}/SQ9IAW.log" && echo Passed || echo Failed
+
+cp $TMP "$COOKIE_FILENAME" 
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
+}
+
+
+t006_cookie_validation() {
+TMP=$(mktemp)
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
+cp  "$COOKIE_FILENAME" $TMP
+cp files/invalid_cookie.txt  "$COOKIE_FILENAME"
+../src/get-callsign-info-page.bash SQ9IAW  > /dev/null
+[ ! -e ${DB_DIRECTORY}/SQ9IAW.log ] && echo Passed || echo Failed
+cp $TMP "$COOKIE_FILENAME" 
+rm -f ${DB_DIRECTORY}/SQ9IAW.log
+}
+
 
 main() {
 cd $(dirname $0)
@@ -42,6 +64,8 @@ t001_script_runned_without_arguments_return_1
 t002_script_has_this_callsign_in_db__is_it_consious_of_that
 t003_script_hasNOT_this_callsign_in_db__
 t004_script_hasNOT_this_callsign_in_db__
+t005_cookie_validation
+t006_cookie_validation
 }
 
 main 
