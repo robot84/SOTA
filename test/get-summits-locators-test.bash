4dd2d0e4bf0b458f50354e3dcbaea016eac805d8
robot84 <zabkiewicz.robert@gmail.com>
2018-11-04 06:19:07 +0100

    Refactoring. File structure on functions now.

--- test/get-summits-locators-test.bash
+++ test/get-summits-locators-test.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 #
 #
-# Version 0.0.1
+. ../src/load_config_file.bash
 
 PASSED_COUNT=0
 FAILED_COUNT=0
 
+function t001_test() {
 TEST_NAME="wc1"
-RESULT=`../get-summits-locators.bash files/region.tmp | wc -l`
+cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
+rm "$SUMMITS_QTH_LOCATORS_FILE"
+RESULT=`../src/get-summits-locators.bash ../test/files/region.tmp | wc -l`
 if [ $RESULT -eq 74 ]
 then
 echo Test \'$TEST_NAME\' PASSED
@@ -16,9 +19,14 @@ else
 echo Test \'$TEST_NAME\' FAILED
 (( FAILED_COUNT = FAILED_COUNT + 1 ))
 fi
+cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE" 
+}
 
+function t002_test() {
 TEST_NAME="wc2"
-RESULT=`../get-summits-locators.bash files/region.tmp | grep -cP '\w+/\w+-\d+ \w\w\d\d\w\w'`
+cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
+rm "$SUMMITS_QTH_LOCATORS_FILE"
+RESULT=`../src/get-summits-locators.bash ../test/files/region.tmp | grep -cP '\w+/\w+-\d+ \w\w\d\d\w\w'`
 if [ $RESULT -eq 74 ]
 then
 echo Test \'$TEST_NAME\' PASSED
@@ -27,9 +35,14 @@ else
 echo Test \'$TEST_NAME\' FAILED
 (( FAILED_COUNT = FAILED_COUNT + 1 ))
 fi
+cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE" 
+}
 
+function t003_test() {
 TEST_NAME="wc3"
-RESULT=`../get-summits-locators.bash files/region.tmp | grep -cPv '\w+/\w+-\d+ \w\w\d\d\w\w'`
+cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
+rm "$SUMMITS_QTH_LOCATORS_FILE"
+RESULT=`../src/get-summits-locators.bash ../test/files/region.tmp | grep -cPv '\w+/\w+-\d+ \w\w\d\d\w\w'`
 if [ $RESULT -eq 0 ]
 then
 echo Test \'$TEST_NAME\' PASSED
@@ -38,18 +51,35 @@ else
 echo Test \'$TEST_NAME\' FAILED
 (( FAILED_COUNT = FAILED_COUNT + 1 ))
 fi
+cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE" 
+}
 
+function print_summary() {
 (( ALL_TESTS_EXECUTED = PASSED_COUNT + FAILED_COUNT ))
 echo
 echo Summary:
 echo Tests executed:  $ALL_TESTS_EXECUTED
 echo Passed: $PASSED_COUNT
 echo Failed: $FAILED_COUNT
+}
 
+function end() {
 if [ $FAILED_COUNT -eq 0 ]
 then
 exit 0
 else
 exit $FAILED_COUNT
 fi
+}
 
+
+function main() {
+t001_test
+t002_test
+t003_test
+print_summary
+end
+}
+
+
+main 
