51d0600548d303dd0047208ee19192e3169bce63
robot84 <zabkiewicz.robert@gmail.com>
2018-11-06 21:24:44 +0100

    Indent

--- test/get-callsign-info-page-test.bash
+++ test/get-callsign-info-page-test.bash
@@ -1,70 +1,70 @@
 #!/bin/bash
 
 t001_script_runned_without_arguments_return_1() {
-../src/get-callsign-info-page.bash > /dev/null 2> /dev/null \
-&& echo Failed || echo Passed
-rm  -f ${DB_DIRECTORY}/SQ9IAW.log
+  ../src/get-callsign-info-page.bash > /dev/null 2> /dev/null \
+  && echo Failed || echo Passed
+  rm  -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 
 t002_script_has_this_callsign_in_db__is_it_consious_of_that() {
-cp files/SQ9IAW.log ${DB_DIRECTORY}
-../src/get-callsign-info-page.bash SQ9IAW | grep \
-"Nothing to do." > /dev/null && echo Passed || echo Failed
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  cp files/SQ9IAW.log ${DB_DIRECTORY}
+  ../src/get-callsign-info-page.bash SQ9IAW | grep \
+  "Nothing to do." > /dev/null && echo Passed || echo Failed
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 t003_script_hasNOT_this_callsign_in_db__() {
-
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
-../src/get-callsign-info-page.bash SQ9IAW | grep -v "Nothing to do." > /dev/null && echo Passed || echo Failed
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  ../src/get-callsign-info-page.bash SQ9IAW | grep -v "Nothing to do." > /dev/null && echo Passed || echo Failed
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 t004_script_hasNOT_this_callsign_in_db__() {
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
-../src/get-callsign-info-page.bash SQ9IAW | grep -q \
-"Getting info about" > /dev/null && echo Passed || echo Failed
-
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  ../src/get-callsign-info-page.bash SQ9IAW | grep -q \
+  "Getting info about" > /dev/null && echo Passed || echo Failed
+  
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 t005_cookie_validation() {
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
-TMP=$(mktemp)
-cp  "$COOKIE_FILENAME" $TMP
-cp files/good_cookie.txt  "$COOKIE_FILENAME"
-../src/get-callsign-info-page.bash SQ9IAW  > /dev/null
-grep -q JO90xc "${DB_DIRECTORY}/SQ9IAW.log" && \
-grep -q Logout "${DB_DIRECTORY}/SQ9IAW.log" && echo Passed || echo Failed
-
-cp $TMP "$COOKIE_FILENAME" 
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  TMP=$(mktemp)
+  cp  "$COOKIE_FILENAME" $TMP
+  cp files/good_cookie.txt  "$COOKIE_FILENAME"
+  ../src/get-callsign-info-page.bash SQ9IAW  > /dev/null
+  grep -q JO90xc "${DB_DIRECTORY}/SQ9IAW.log" && \
+  grep -q Logout "${DB_DIRECTORY}/SQ9IAW.log" && echo Passed || echo Failed
+  
+  cp $TMP "$COOKIE_FILENAME"
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 
 t006_cookie_validation() {
-TMP=$(mktemp)
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
-cp  "$COOKIE_FILENAME" $TMP
-cp files/invalid_cookie.txt  "$COOKIE_FILENAME"
-../src/get-callsign-info-page.bash SQ9IAW  > /dev/null
-[ ! -e ${DB_DIRECTORY}/SQ9IAW.log ] && echo Passed || echo Failed
-cp $TMP "$COOKIE_FILENAME" 
-rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  TMP=$(mktemp)
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
+  cp  "$COOKIE_FILENAME" $TMP
+  cp files/invalid_cookie.txt  "$COOKIE_FILENAME"
+  ../src/get-callsign-info-page.bash SQ9IAW  > /dev/null
+  [ ! -e ${DB_DIRECTORY}/SQ9IAW.log ] && echo Passed || echo Failed
+  cp $TMP "$COOKIE_FILENAME"
+  rm -f ${DB_DIRECTORY}/SQ9IAW.log
 }
 
 
 main() {
-cd $(dirname $0)
-. ../resources/config.cfg
-
-t001_script_runned_without_arguments_return_1
-t002_script_has_this_callsign_in_db__is_it_consious_of_that
-t003_script_hasNOT_this_callsign_in_db__
-t004_script_hasNOT_this_callsign_in_db__
-t005_cookie_validation
-t006_cookie_validation
+  cd $(dirname $0)
+  . ../resources/config.cfg
+  
+  t001_script_runned_without_arguments_return_1
+  t002_script_has_this_callsign_in_db__is_it_consious_of_that
+  t003_script_hasNOT_this_callsign_in_db__
+  t004_script_hasNOT_this_callsign_in_db__
+  t005_cookie_validation
+  t006_cookie_validation
 }
 
-main 
+main
--- test/get-summits-locators-test.bash
+++ test/get-summits-locators-test.bash
@@ -8,82 +8,82 @@ PASSED_COUNT=0
 FAILED_COUNT=0
 
 function t001_test() {
-TEST_NAME="wc1"
-cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
-rm "$SUMMITS_QTH_LOCATORS_FILE"
-../src/get-summits-locators.bash ../test/files/region.tmp
-RESULT=`cat "$SUMMITS_QTH_LOCATORS_FILE" | wc -l`
-if [ $RESULT -eq 74 ]
-then
-echo Test \'$TEST_NAME\' PASSED
-(( PASSED_COUNT = PASSED_COUNT + 1 ))
-else
-echo Test \'$TEST_NAME\' FAILED
-(( FAILED_COUNT = FAILED_COUNT + 1 ))
-fi
-cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE" 
+  TEST_NAME="wc1"
+  cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
+  rm "$SUMMITS_QTH_LOCATORS_FILE"
+  ../src/get-summits-locators.bash ../test/files/region.tmp
+  RESULT=`cat "$SUMMITS_QTH_LOCATORS_FILE" | wc -l`
+  if [ $RESULT -eq 74 ]
+  then
+    echo Test \'$TEST_NAME\' PASSED
+    (( PASSED_COUNT = PASSED_COUNT + 1 ))
+  else
+    echo Test \'$TEST_NAME\' FAILED
+    (( FAILED_COUNT = FAILED_COUNT + 1 ))
+  fi
+  cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE"
 }
 
 function t002_test() {
-TEST_NAME="wc2"
-cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
-rm "$SUMMITS_QTH_LOCATORS_FILE"
-../src/get-summits-locators.bash ../test/files/region.tmp
-RESULT=`cat "$SUMMITS_QTH_LOCATORS_FILE"  | grep -cP '\w+/\w+-\d+ \w\w\d\d\w\w'`
-if [ $RESULT -eq 74 ]
-then
-echo Test \'$TEST_NAME\' PASSED
-(( PASSED_COUNT = PASSED_COUNT + 1 ))
-else
-echo Test \'$TEST_NAME\' FAILED
-(( FAILED_COUNT = FAILED_COUNT + 1 ))
-fi
-cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE" 
+  TEST_NAME="wc2"
+  cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
+  rm "$SUMMITS_QTH_LOCATORS_FILE"
+  ../src/get-summits-locators.bash ../test/files/region.tmp
+  RESULT=`cat "$SUMMITS_QTH_LOCATORS_FILE"  | grep -cP '\w+/\w+-\d+ \w\w\d\d\w\w'`
+  if [ $RESULT -eq 74 ]
+  then
+    echo Test \'$TEST_NAME\' PASSED
+    (( PASSED_COUNT = PASSED_COUNT + 1 ))
+  else
+    echo Test \'$TEST_NAME\' FAILED
+    (( FAILED_COUNT = FAILED_COUNT + 1 ))
+  fi
+  cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE"
 }
 
 function t003_test() {
-TEST_NAME="wc3"
-cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
-rm "$SUMMITS_QTH_LOCATORS_FILE"
-../src/get-summits-locators.bash ../test/files/region.tmp
-RESULT=`cat "$SUMMITS_QTH_LOCATORS_FILE"  | grep -cPv '\w+/\w+-\d+ \w\w\d\d\w\w'`
-if [ $RESULT -eq 0 ]
-then
-echo Test \'$TEST_NAME\' PASSED
-(( PASSED_COUNT = PASSED_COUNT + 1 ))
-else
-echo Test \'$TEST_NAME\' FAILED
-(( FAILED_COUNT = FAILED_COUNT + 1 ))
-fi
-cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE" 
+  TEST_NAME="wc3"
+  cp "$SUMMITS_QTH_LOCATORS_FILE" ../test/files/summits_locators.backup
+  rm "$SUMMITS_QTH_LOCATORS_FILE"
+  ../src/get-summits-locators.bash ../test/files/region.tmp
+  RESULT=`cat "$SUMMITS_QTH_LOCATORS_FILE"  | grep -cPv '\w+/\w+-\d+ \w\w\d\d\w\w'`
+  if [ $RESULT -eq 0 ]
+  then
+    echo Test \'$TEST_NAME\' PASSED
+    (( PASSED_COUNT = PASSED_COUNT + 1 ))
+  else
+    echo Test \'$TEST_NAME\' FAILED
+    (( FAILED_COUNT = FAILED_COUNT + 1 ))
+  fi
+  cp ../test/files/summits_locators.backup "$SUMMITS_QTH_LOCATORS_FILE"
 }
 
 function print_summary() {
-(( ALL_TESTS_EXECUTED = PASSED_COUNT + FAILED_COUNT ))
-echo
-echo Summary:
-echo Tests executed:  $ALL_TESTS_EXECUTED
-echo Passed: $PASSED_COUNT
-echo Failed: $FAILED_COUNT
+  (( ALL_TESTS_EXECUTED = PASSED_COUNT + FAILED_COUNT ))
+  echo
+  echo Summary:
+  echo Tests executed:  $ALL_TESTS_EXECUTED
+  echo Passed: $PASSED_COUNT
+  echo Failed: $FAILED_COUNT
 }
 
 function end() {
-if [ $FAILED_COUNT -eq 0 ]
-then
-exit 0
-else
-exit $FAILED_COUNT
-fi
+  if [ $FAILED_COUNT -eq 0 ]
+  then
+    exit 0
+  else
+    exit $FAILED_COUNT
+  fi
 }
 
 
 function main() {
-t001_test
-t002_test
-t003_test
-print_summary
-end
+  t001_test
+  t002_test
+  t003_test
+  print_summary
+  end
 }
 
 
-main 
+main
