f00f3fff10324f955e5c9fd680b6e8c5e5dcc383
robot84 <zabkiewicz.robert@gmail.com>
2018-11-17 06:50:06 +0100

    Added all Polish archival summits.

--- tools/archival_summits_tools/list_archival_summits.bash
+++ tools/archival_summits_tools/list_archival_summits.bash
@@ -1,12 +1,40 @@
-y=1
-for i in {01..79}
+function parse_parameters() {
+	if [ "$#" -lt 1 ]
+		then
+			echo
+			echo "Usage:"
+			echo "$0 <region-info-file-name>"
+			echo
+			echo "Please pass as the argument name of a file containing information about summits"
+			echo "in format:"
+			echo "OK/PO-001 JO90wa"
+			echo "OK/PO-002 JN99aa"
+			echo " ... "
+			echo ""
+			echo "You can use resource/summits_database.dat file"
+			exit 1
+			fi
+}
+
+
+parse_parameters $@
+declare -i last_num=0
+declare -i new_num=0
+input_file="$1"
+lines=$(cat "$input_file" | wc -l)
+ref_prefix=$(sed -rn "1 s/[[:digit:]]{3} .*//p" "$input_file")
+for i in $(seq 1 $lines)
 do
-x=$(sed -n "$i p" summits.num)
-echo x: $x y: $y
-if [ $x -ne $y ]
+new_num=10#$(sed  -rn "$i s/.*([[:digit:]]{3}).*/\1/p" "$input_file")
+# echo new: $new_num last: $last_num
+if [ $new_num -ne $(( $last_num + 1 )) ]
 then
-echo ta liczba: $y
-y=$(( $y + 1 ))
+# echo last1 $(( $last_num + 1 )) new $(( $new_num - 1 ))
+for ii in $(seq $(( $last_num + 1 )) $(( $new_num - 1 )) )
+do
+printf "%s%03d\n" $ref_prefix $ii
+# printf "%s%03u\n" $ref_prefix $(( $y + 1 ))
+done
 fi
-y=$(( $y + 1 ))
+last_num=$new_num
 done
