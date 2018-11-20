93e257201ff088d22da55260f49359910f0d305b
robot84 <zabkiewicz.robert@gmail.com>
2018-11-20 19:12:22 +0100

    Added if_file_exists function

--- src/load_config_file.bash
+++ src/load_config_file.bash
@@ -1,6 +1,27 @@
 #!/bin/bash
 ERROR__CANNOT_LOAD_CONFIG_FILE=100
 
+function if_file_exists() {
+  if [ ! -r "$1" ]
+  then
+    echo "ERROR: Cannot open ${2:- } file for reading!"
+    echo "Check if file exists and you have permissions to read it."
+    echo "You tried to find: $(readlink -m $1)"
+	local proposition=
+ 	[ -d " $(dirname $(readlink -m $1))" ] && \
+	proposition=$(find $(dirname $(readlink -m $1)) -name $(basename $1) -print)
+	[ ! -z $proposition ] && echo "Maybe you are looking for $proposition ?"
+	proposition=
+	[ -d "../$(dirname $1)" ] && \
+ 	proposition=$(find  ../$(dirname $1) -name $(basename $1) -print)
+	[ ! -z $proposition ] && echo "Maybe you are looking for $proposition ?"
+    echo "Cannot continue. Exiting..."
+    exit $ERROR__CANNOT_LOAD_CONFIG_FILE
+  else
+    return 0
+  fi
+}
+
 
 function if_config_file_exists() {
 CONFIG_FILE="$1/../resources/config.cfg"
@@ -16,11 +37,6 @@ CONFIG_FILE="$1/../resources/config.cfg"
   fi
 }
 
-
-function load_config_file() {
-  if_config_file_exists "$1"
-  .  ${CONFIG_FILE}
-}
 
 # temporary obsolete
 function set_working_dir() {
@@ -42,3 +58,10 @@ function restore_original_dir() {
     cd "$ACTUAL_DIR" > /dev/null
   fi
 }
+
+function load_config_file() {
+  if_config_file_exists "$1"
+  .  ${CONFIG_FILE}
+}
+
+
