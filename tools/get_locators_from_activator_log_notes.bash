c3b1d788e980b1e5dfeba1c02e419f3b4a87ff53
robot84 <zabkiewicz.robert@gmail.com>
2018-11-20 19:11:16 +0100

    Show only option added

--- tools/get_locators_from_activator_log_notes.bash
+++ tools/get_locators_from_activator_log_notes.bash
@@ -5,6 +5,11 @@
 #Second - alternate method
 #cat SO9ARC-all-qsos.csv  | grep -P "\w\w\d\d\w\w" | cut -d, -f8- | sed 's/,,/ /' | sed -E 's/(\w*) (.*)([[:alpha:]]{2}[[:digit:]]{2}[[:alpha:]]{2})(.*)/\1 \3/'
 
+
+
+SHOW_ONLY_ENABLED=no
+ACTIVATOR_LOG=""
+
 function parse_parameters() {
  POSITIONAL=()
   while [[ $# -gt 0 ]]
@@ -13,9 +18,10 @@ function parse_parameters() {
     
     case $key in
       -h|--help)
-        echo "Usage: ${0##*/} <activator_log_file.csv>"
+        echo "Usage: ${0##*/} [-s] <activator_log_file.csv>"
         echo "Searching QTH Locators for callsigns in report notes of activator's log."
         echo
+		echo -e "      -s|--show-only\t\t only show locators found in activator log. Do not add them to database"
 		echo -e "      --help\t\t\t display this help and exit"
 		echo -e "      --version\t\t\t output version information and exit"
         exit 1
@@ -30,6 +36,10 @@ function parse_parameters() {
 		echo "Written by Robert Zabkiewicz SO9ARC."
         exit
       ;;
+	  -s|--show-only)
+	  SHOW_ONLY_ENABLED=yes
+	  shift
+	  ;;
       -*)
         echo "$0: invalid option -- '$1'"
         echo "Try '$0 --help' for more information."
@@ -43,6 +53,8 @@ function parse_parameters() {
   done
   set -- "${POSITIONAL[@]}" # restore positional parameters
 
+  ACTIVATOR_LOG="${POSITIONAL[0]}"
+
    if [ "$#" -lt 1 ]
   then
     echo "$0: Mandatory argument ommited."
@@ -50,31 +62,36 @@ function parse_parameters() {
     f_log_msg "$ERROR_LOG_FILE" "No activator log passed to script."
     exit 1
   fi
-  
-  
-# activator_log="${POSITIONAL[0]}"
 }
+
 
 function change_all_letters_in_file_to_uppercase() {
 sed -ibackup -e 's/.*/\U&/' "$1"
 }
 
+
 function create_file_with_all_callsigns_which_have_locators_written_in_notes_field_in_activator_log() {
 local activator_log="$1"
 local output_file="$2"
+if_file_exists "$activator_log" "activator log"
 cat "$activator_log" | \
 grep -P "\w\w\d\d\w\w" | cut -d, -f8- | sed 's/,,/ /' | sed -r "s/([[:alpha:]]{2}[[:digit:]]{2}[[:alpha:]]{2})|(^\S*)/,&,/g" |  cut -d, -f2,4 | tr  "," " " | grep -v "/" | sort | uniq | \
 tr "[[:lower:]]" "[[:upper:]]" > "$output_file"
-
 }
 
 
 function main() {
 
 file_with_all_callsigns_with_locator_in_notes_field_from_activator_log=$(mktemp)
-create_file_with_all_callsigns_which_have_locators_written_in_notes_field_in_activator_log "$1" "$file_with_all_callsigns_with_locator_in_notes_field_from_activator_log"
+create_file_with_all_callsigns_which_have_locators_written_in_notes_field_in_activator_log "$ACTIVATOR_LOG" "$file_with_all_callsigns_with_locator_in_notes_field_from_activator_log"
 change_all_letters_in_file_to_uppercase "$file_with_all_callsigns_with_locator_in_notes_field_from_activator_log"
 change_all_letters_in_file_to_uppercase "$CHASERS_QTH_LOCATORS_FILE"
+
+if [ $SHOW_ONLY_ENABLED = "yes" ]
+then
+cat "$file_with_all_callsigns_with_locator_in_notes_field_from_activator_log"
+exit 0
+fi
 
 
 while read qra qth
@@ -82,8 +99,7 @@ do
 	grep $qra "$CHASERS_QTH_LOCATORS_FILE" > /dev/null || \
 	{ 
 	echo "No $qra in database... Adding..."
-
-		 echo $qra $qth >> "$CHASERS_QTH_LOCATORS_FILE" 
+	echo $qra $qth >> "$CHASERS_QTH_LOCATORS_FILE" 
 	}
 done < "$file_with_all_callsigns_with_locator_in_notes_field_from_activator_log"
 
@@ -95,4 +111,4 @@ this_script_dir=$(dirname $0)
 . "$this_script_dir/../src/load_config_file.bash"
 load_config_file "$this_script_dir"
 parse_parameters $@
-main $@
+main
